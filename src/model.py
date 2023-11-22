60922741ee23d94a5d24e8d6d6bd9b3f4354b92d
felipelamarca <felipe.lamarca@hotmail.com>
2023-11-22 16:14:29 -0300

    Change database structure

--- src/model.py
+++ src/model.py
@@ -9,53 +9,105 @@ class BlogModel:
         self.cursor = self.connection.cursor()
 
     def drop_table(self):
-        self.cursor.execute('DROP TABLE IF EXISTS *')
+        self.cursor.execute('DROP TABLE IF EXISTS users CASCADE')
         self.connection.commit()
 
-    def create_table(self):
+    def create_table(self):      
+        #User table
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS users (
-                id SERIAL PRIMARY KEY,
-                name TEXT,
-                email TEXT
+                id INTEGER serial PRIMARY KEY,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                email TEXT NOT NULL,
+                admin BOOLEAN DEFAULT FALSE
             )
+        ''')
+
+        #Unique identifier for posts and comments
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS postIdentifier (
+                id SERIAL PRIMARY KEY,
+                author_id INTEGER REFERENCES users(id),
+                date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             ''')
+
+        #Post table
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS posts (
-                id SERIAL PRIMARY KEY,
-                title TEXT,
+                id INTEGER REFERENCES postIdentifier(id),
+                title TEXT NOT NULL,
                 content TEXT,
-                author_id INTEGER REFERENCES users(id),
-                date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                image TEXT
             )
         ''')
+
+        #Comment table
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS comments (
-                id SERIAL PRIMARY KEY,
+                id INTEGER REFERENCES postIdentifier(id),
                 content TEXT,
-                parent_post_id INTEGER REFERENCES posts(id),
-                author_id INTEGER REFERENCES users(id),
-                date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            )
-        ''')
-        self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS users (
-                id SERIAL PRIMARY KEY,
-                username TEXT,
-                password TEXT,
-                email TEXT
+                image TEXT,
+                parent_post_id INTEGER REFERENCES posts(id)
             )
         ''')
 
+        #Tags table
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS tags (
+                id SERIAL PRIMARY KEY,
+                name TEXT UNIQUE NOT NULL
+            )
+        ''')
+
+        #Relation table for likes and users
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS postLikes (
+                id INTEGER REFERENCES postIdentifier(id),
+                user_id INTEGER REFERENCES users(id),
+                UNIQUE(id, user_id)
+            )
+        ''')
+
+        #Relation table for followers
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS userFollows (
+                id INTEGER REFERENCES users(id)
+                follower_id INTEGER REFERENCES users(id),
+                UNIQUE(id, follower_id)
+            )
+        ''')
+
+        #Relation for banned users
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS bannedUsers (
+                id INTEGER REFERENCES users(id),
+                banned_id INTEGER REFERENCES users(id),
+                UNIQUE(id, banned_id)
+            )
+        ''')
+
+        #Relation for tags and posts
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS postTags (
+                id INTEGER REFERENCES postIdentifier(id),
+                tag_id INTEGER REFERENCES tags(id),
+                UNIQUE(id, tag_id)
+            )
+        ''')
+
+        # create a new user
+        self.cursor.execute('INSERT INTO users (name, email, password, admin) VALUES (%s, %s, %s, %s)', ('admin', 'admin', 'admin', True))
+        
         self.connection.commit()
 
 
-    def create_user(self, name, email):
-        self.cursor.execute('INSERT INTO users (name, email) VALUES (%s, %s) RETURNING id', (name, email))
+    def create_user(self, user):
+        self.cursor.execute('INSERT INTO users (name, email, password, banned) VALUES (%s, %s, %s, %s) RETURNING id', (user.username, user.email, user.password, user.banned))
         self.connection.commit()
         fetched = self.cursor.fetchone()
-        #user.set_user_id(fetched[0])
-        #return user
+        user.set_user_id(fetched[0])
+        return user
 
     def create_post(self, post):
         self.cursor.execute('INSERT INTO posts (title, content, author_id) VALUES (%s, %s, %s) RETURNING id, date', (post.title, post.content, post.author_id))
@@ -87,7 +139,8 @@ class BlogModel:
     def check_user(self, username, password):
         self.cursor.execute('SELECT * FROM users WHERE username = %s AND password = %s', (username, password))
         return bool(self.cursor.fetchone())
-    
-# bg = BlogModel()
-# bg.create_table()
-# bg.check_user('admin', 'admin')
+
+bg = BlogModel()
+#bg.drop_table()
+bg.create_table()
+bg.check_user('admin', 'admin')
