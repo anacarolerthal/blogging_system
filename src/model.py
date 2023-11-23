a76b6f784405b7fd4331539975211362b8c75b86
Guilherme de Melo <84725801+guilherme-melo@users.noreply.github.com>
2023-11-22 22:50:30 -0300

    Modeling DB
    Co-Authored-By: Paloma Vieira Borges <84725592+palomavb@users.noreply.github.com>

--- src/model.py
+++ src/model.py
@@ -9,13 +9,82 @@ class BlogModel:
         self.cursor = self.connection.cursor()
 
     def create_table(self):
+
+        #User table
         self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS posts (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                title TEXT,
-                content TEXT
+            CREATE TABLE IF NOT EXISTS users (
+                user_id INTEGER serial PRIMARY KEY,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                email TEXT NOT NULL,
+                admin BOOLEAN DEFAULT FALSE
             )
         ''')
+
+        #Post table
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS posts (
+                post_id SERIAL PRIMARY KEY,
+                author_id INTEGER REFERENCES users(user_id),
+                date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                title TEXT NOT NULL,
+                content TEXT,
+                image TEXT,
+                parent_post_id INTEGER REFERENCES posts(id),
+                CHECK (content IS NOT NULL OR image IS NOT NULL)
+            )
+        ''')
+
+        #Tags table
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS tags (
+                tag_id SERIAL PRIMARY KEY,
+                name TEXT UNIQUE NOT NULL
+            )
+        ''')
+
+        #Relation table for likes
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS postLikes (
+                like_id SERIAL PRIMARY KEY,
+                post_id INTEGER REFERENCES posts(post_id),
+                user_id INTEGER REFERENCES users(user_id),
+                UNIQUE(post_id, user_id)
+            )
+        ''')
+
+        #Relation table for followers
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS userFollows (
+                follow_id SERIAL PRIMARY KEY,
+                follower_id INTEGER REFERENCES users(user_id),
+                followee_id INTEGER REFERENCES users(user_id),
+                UNIQUE(follower_id, followee_id)
+            )
+        ''')
+
+        #Relation for banned users
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS bannedUsers (
+                ban_id SERIAL PRIMARY KEY,
+                banner_id INTEGER REFERENCES users(user_id),
+                banned_id INTEGER REFERENCES users(user_id) UNIQUE
+            )
+        ''')
+
+        #Relation for tags and posts
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS postTags (
+                tag_post_id SERIAL PRIMARY KEY,
+                tag_id INTEGER REFERENCES tags(tag_id),
+                post_id INTEGER REFERENCES posts(post_id),
+                UNIQUE(tag_id, post_id)
+            )
+        ''')
+
+        # create a new user
+        self.cursor.execute('INSERT INTO users (name, email, password, admin) VALUES (%s, %s, %s, %s)', ('admin', 'admin', 'admin', True))
+
         self.connection.commit()
 
     def create_post(self, title, content):
