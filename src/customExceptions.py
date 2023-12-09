fb884cea62dbb6a86b2a9803f9283a1e7b1cb6d2
Guilherme de Melo <84725801+guilherme-melo@users.noreply.github.com>
2023-12-09 02:23:02 -0300

    forgot to commit the excpetions oops2
    Co-Authored-By: Ana Carolina Erthal <81707790+anacarolerthal@users.noreply.github.com>

--- src/customExceptions.py
+++ src/customExceptions.py
@@ -16,9 +16,28 @@ class AlreadyFollowing(Exception):
 class AlreadyNotFollowing(Exception):
     def __init__(self) -> None:
         super().__init__("Não é possível deixar de seguir novamente o mesmo usuário.")
+        
 class FollowInvalidUser(Exception):
     def __init__(self) -> None:
         super().__init__("Não é possível seguir usuário inválido.")
+
 class UnfollowInvalidUser(Exception):
     def __init__(self) -> None:
         super().__init__("Não é possível deixar de seguir usuário inválido.")
+
+class InvalidUserException(Exception):
+    def __init__(self) -> None:
+        super().__init__("O usuário não existe.")
+
+class InvalidPostException(Exception):
+    def __init__(self) -> None:
+        super().__init__("O post não existe.")
+
+class AlreadyLiked(Exception):
+    def __init__(self) -> None:
+        super().__init__("Post já curtido.")
+
+class AlreadyNotLiked(Exception):
+    def __init__(self) -> None:
+        super().__init__("Post já descurtido.")
+
