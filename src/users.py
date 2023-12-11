4567f442bf6aa1fa160b2a4c86fb79e033e6f87e
Ana Carolina Erthal <acarolerthal@gmail.com>
2023-12-10 21:22:44 -0300

    comments for user

--- src/users.py
+++ src/users.py
@@ -43,14 +43,15 @@ class BaseUser(ABC):
         """Get the id of the user
 
         Returns:
-        int -> id of the user"""
+        self.id: int -> id of the user
+        """
         return self.id
 
     def get_username(self) -> str:
         """Get the username of the user
 
         Returns:
-        str -> username of the user
+        self.username: str -> username of the user
         """
         return self.username
 
@@ -58,13 +59,22 @@ class BaseUser(ABC):
         """Get the email of the user
 
         Returns:
-        str -> email of the user
+        self.email: str -> email of the user
         """
         return self.email
 
 
 class User(BaseUser):
     """A user of the system
+
+    Attributes:
+    id: int -> id of the user
+    username: str -> username of the user
+    password: str -> password of the user
+    email: str -> email of the user
+    posts: List[int] -> list of the posts of the user
+    followers: List[int] -> list of the followers of the user
+    following: List[int] -> list of the following of the user
     """
     def __init__(self,
                  username: str,
@@ -83,31 +93,55 @@ class User(BaseUser):
         self.__following = following if following is not None else []
 
     def get_posts(self) -> List[int]:
-        """Get the posts of the user"""
+        """Get the posts of the user
+
+        Returns:
+        self.__posts: List[int] -> list of the posts of the user
+        """
         return self.__posts
 
     def get_followers(self) -> List[int]:
-        """Get the followers of the user"""
+        """Get the followers of the user
+
+        Returns:
+        self.__followers: List[int] -> list of the followers of the user
+        """
         id_self = self.get_id()
         follower_list = BlogModel().get_followers_user(id_self)
         self.__followers = [user for user in follower_list]
         return self.__followers
 
     def get_following(self) -> List[int]:
-        """Get the following of the user"""
+        """Get the following of the user
+
+        Returns:
+        self.__following: List[int] -> list of the following of the user
+        """
         id_self = self.get_id()
         following_list = BlogModel().get_following_user(id_self)
         self.__following = [user for user in following_list]
         return self.__following
 
     def get_liked_posts(self) -> List[int]:
-        """Get the liked posts of the user"""
+        """Get the liked posts of the user
+
+        Returns:
+        liked_posts: List[int] -> list of the liked posts of the user
+        """
         id_self = self.get_id()
         liked_posts = BlogModel().get_all_liked_posts_by_user(id_self)
         return liked_posts
 
     def like(self, post_id: int) -> None:
-        """Like a post"""
+        """Like a post
+
+        Args:
+        post_id: int -> id of the post to be liked
+
+        Raises:
+        InvalidPostException -> if the post does not exist
+        AlreadyLiked -> if the post is already liked
+        """
         if not BlogModel().check_if_post_in_db(post_id):
             raise InvalidPostException()
 
@@ -118,7 +152,15 @@ class User(BaseUser):
             BlogModel().like(self.get_id(), post_id)
 
     def unlike(self, post_id: int) -> None:
-        """Unlike a post"""
+        """Unlike a post
+
+        Args:
+        post_id: int -> id of the post to be unliked
+
+        Raises:
+        InvalidPostException -> if the post does not exist
+        AlreadyNotLiked -> if the post is already not liked
+        """
         if not BlogModel().check_if_post_in_db(post_id):
             raise InvalidPostException()
 
@@ -129,7 +171,16 @@ class User(BaseUser):
 
 
     def follow(self, followee_id: int) -> None:
-        """Follow a user"""
+        """Follow a user
+
+        Args:
+        followee_id: int -> id of the user to be followed
+
+        Raises:
+        InvalidUserException -> if the user does not exist
+        CannotFollowSelf -> if the user tries to follow himself
+        AlreadyFollowing -> if the user is already following the user
+        """
         if not BlogModel().check_if_user_in_db(followee_id):
             raise InvalidUserException()
 
@@ -144,7 +195,16 @@ class User(BaseUser):
             listener.post_event("follow")
 
     def unfollow(self, followee_id: int) -> None:
-        """Unfollow a user"""
+        """Unfollow a user
+
+        Args:
+        followee_id: int -> id of the user to be unfollowed
+
+        Raises:
+        InvalidUserException -> if the user does not exist
+        CannotUnfollowSelf -> if the user tries to unfollow himself
+        AlreadyNotFollowing -> if the user is already not following the user
+        """
 
         if not BlogModel().check_if_user_in_db(followee_id):
             raise InvalidUserException()
@@ -160,7 +220,14 @@ class User(BaseUser):
         pass
 
 class Moderator(BaseUser):
-    """A moderator of the system"""
+    """A moderator of the system
+
+    Attributes:
+    id: int -> id of the user
+    username: str -> username of the user
+    password: str -> password of the user
+    email: str -> email of the user
+    """
     def __init__(self,
                  username: str,
                  password: str = None,
@@ -172,14 +239,29 @@ class Moderator(BaseUser):
         self.email = email
 
     def delete_post(self, post_id: int) -> None:
-        """Delete a post"""
+        """Delete a post
+
+        Args:
+        post_id: int -> id of the post to be deleted
+
+        Raises:
+        InvalidPostException -> if the post does not exist
+        """
         if not BlogModel().check_if_post_in_db(post_id):
             raise InvalidPostException()
         else:
             BlogModel().delete_post(post_id)
 
     def ban_user(self, banned_user_id: int) -> None:
-        """Ban a user"""
+        """Ban a user
+
+        Args:
+        banned_user_id: int -> id of the user to be banned
+
+        Raises:
+        InvalidUserException -> if the user does not exist
+        AlreadyBanned -> if the user is already banned
+        """
         print(BlogModel().check_if_user_is_banned(banned_user_id))
         if not BlogModel().check_if_user_in_db(banned_user_id):
             raise InvalidUserException()
@@ -190,7 +272,15 @@ class Moderator(BaseUser):
             BlogModel().ban_user(self.id, banned_user_id)
 
     def unban_user(self, unbanned_user_id: int) -> None:
-        """Unban a user"""
+        """Unban a user
+
+        Args:
+        unbanned_user_id: int -> id of the user to be unbanned
+
+        Raises:
+        InvalidUserException -> if the user does not exist
+        AlreadyUnbanned -> if the user is already unbanned
+        """
         print(BlogModel().check_if_user_is_banned(unbanned_user_id))
         if not BlogModel().check_if_user_in_db(unbanned_user_id):
             raise InvalidUserException()
@@ -204,7 +294,20 @@ class UserFactory:
     """Factory class for users"""
     @staticmethod
     def create_user(type_: str, username: str, id:int):
-        """Create a user"""
+        """Create a user
+
+        Args:
+        type_: str -> type of the user
+        username: str -> username of the user
+        id: int -> id of the user
+
+        Returns:
+        User -> user created
+        Moderator -> moderator created
+
+        Raises:
+        ValueError -> if the type of the user is not valid
+        """
         if type_ == 'USER':
             return User(username,
                              id)
